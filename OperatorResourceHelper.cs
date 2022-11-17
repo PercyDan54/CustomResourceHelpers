f856c6f2eb37d62f792457af0a12959c02808b20
Baka632 <stevemc123456@outlook.com>
2022-11-17 15:54:55 +0800

    为未来的.NET版本做准备

--- OperatorResourceHelper.cs
+++ OperatorResourceHelper.cs
@@ -7,6 +7,9 @@ namespace ArknightsResources.CustomResourceHelpers
     /// <summary>
     /// 定义一个用以读取与干员相关资源的基类
     /// </summary>
+#if NET7_0_OR_GREATER
+    [System.Obsolete("在.NET 7及以上版本, 请使用IOperatorResourceHelper接口")]
+#endif
     public abstract class OperatorResourceHelper
     {
         /// <summary>
@@ -69,4 +72,74 @@ namespace ArknightsResources.CustomResourceHelpers
         /// <returns>一个<see cref="Operator"/>数组</returns>
         public abstract Task<OperatorsList> GetAllOperatorsAsync(CultureInfo cultureInfo);
     }
+
+#if NET7_0_OR_GREATER
+
+    /// <summary>
+    /// 定义一套用以读取与干员相关资源的方法
+    /// </summary>
+    public interface IOperatorResourceHelper
+    {
+        /// <summary>
+        /// 简体中文的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo ChineseSimplifiedCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-CN", false));
+        /// <summary>
+        /// 繁体中文的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo ChineseTraditionalCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-TW", false));
+        /// <summary>
+        /// 英语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo EnglishCultureInfo = CultureInfo.ReadOnly(new CultureInfo("en-US", false));
+        /// <summary>
+        /// 日语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo JapaneseCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ja-JP", false));
+        /// <summary>
+        /// 韩语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo KoreanCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ko-KR", false));
+        /// <summary>
+        /// 意大利语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo ItalianCultureInfo = CultureInfo.ReadOnly(new CultureInfo("it-IT", false));
+
+        /// <summary>
+        /// 通过干员的立绘信息获取其图片
+        /// </summary>
+        /// <param name="illustrationInfo">干员的立绘信息</param>
+        /// <returns>一个byte数组,其中包含了干员的图片信息</returns>
+        public static abstract byte[] GetOperatorImage(OperatorIllustrationInfo illustrationInfo);
+
+        /// <summary>
+        /// 通过干员名称获取其<see cref="Operator"/>对象
+        /// </summary>
+        /// <param name="operatorName">干员名称</param>
+        /// <param name="cultureInfo"><see cref="Operator"/>对象的语言文化</param>
+        /// <returns>一个<see cref="Operator"/>对象</returns>
+        public static abstract Operator GetOperator(string operatorName, CultureInfo cultureInfo);
+
+        /// <summary>
+        /// 通过干员图像代号获取其<see cref="Operator"/>对象
+        /// </summary>
+        /// <param name="operatorImageCodename">干员图像代号</param>
+        /// <param name="cultureInfo"><see cref="Operator"/>对象的语言文化</param>
+        /// <returns>一个<see cref="Operator"/>对象</returns>
+        public static abstract Operator GetOperatorWithImageCodename(string operatorImageCodename, CultureInfo cultureInfo);
+
+        /// <summary>
+        /// 获取全部干员
+        /// </summary>
+        /// <returns>一个<see cref="Operator"/>数组</returns>
+        public static abstract OperatorsList GetAllOperators(CultureInfo cultureInfo);
+
+        /// <summary>
+        /// 异步获取全部干员
+        /// </summary>
+        /// <returns>一个<see cref="Operator"/>数组</returns>
+        public static abstract Task<OperatorsList> GetAllOperatorsAsync(CultureInfo cultureInfo);
+    }
+
+#endif
 }
--- StoryResourceHelper.cs
+++ StoryResourceHelper.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
+﻿using System.Globalization;
 
 namespace ArknightsResources.CustomResourceHelpers
 {
     /// <summary>
     /// 定义一个用以读取与剧情相关资源的基类
     /// </summary>
+#if NET7_0_OR_GREATER
+    [System.Obsolete("在.NET 7及以上版本, 请使用IStoryResourceHelper接口")]
+#endif
     public abstract class StoryResourceHelper
     {
         /// <summary>
@@ -37,6 +37,46 @@ namespace ArknightsResources.CustomResourceHelpers
         /// <param name="codename">剧情文件的代号</param>
         /// <param name="cultureInfo"></param>
         /// <returns>原始剧情文本</returns>
-        public abstract string GetStoryText(string codename, CultureInfo cultureInfo);
+        public abstract string GetStoryRawText(string codename, CultureInfo cultureInfo);
     }
+
+
+#if NET7_0_OR_GREATER
+
+    /// <summary>
+    /// 定义一系列用以读取与剧情相关资源的方法
+    /// </summary>
+    public interface IStoryResourceHelper
+	{
+        /// <summary>
+        /// 简体中文的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo ChineseSimplifiedCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-CN", false));
+        /// <summary>
+        /// 繁体中文的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo ChineseTraditionalCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-TW", false));
+        /// <summary>
+        /// 英语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo EnglishCultureInfo = CultureInfo.ReadOnly(new CultureInfo("en-US", false));
+        /// <summary>
+        /// 日语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo JapaneseCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ja-JP", false));
+        /// <summary>
+        /// 韩语的语言文化信息
+        /// </summary>
+        public static readonly CultureInfo KoreanCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ko-KR", false));
+
+        /// <summary>
+        /// 通过剧情文件的代号获取该剧情的原始剧情文本
+        /// </summary>
+        /// <param name="codename">剧情文件的代号</param>
+        /// <param name="cultureInfo"></param>
+        /// <returns>原始剧情文本</returns>
+        public static abstract string GetStoryRawText(string codename, CultureInfo cultureInfo);
+    }
+
+#endif
 }

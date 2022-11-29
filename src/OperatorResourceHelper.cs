d6ca6904da420837e5abc170d4d9943f747a1c80
Baka632 <stevemc123456@outlook.com>
2022-11-29 19:54:11 +0800

    移除无法正常访问的字段

--- src/OperatorResourceHelper.cs
+++ src/OperatorResourceHelper.cs
@@ -106,31 +106,6 @@ namespace ArknightsResources.CustomResourceHelpers
     public interface IOperatorResourceHelper
     {
         /// <summary>
-        /// 简体中文的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo ChineseSimplifiedCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-CN", false));
-        /// <summary>
-        /// 繁体中文的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo ChineseTraditionalCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-TW", false));
-        /// <summary>
-        /// 英语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo EnglishCultureInfo = CultureInfo.ReadOnly(new CultureInfo("en-US", false));
-        /// <summary>
-        /// 日语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo JapaneseCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ja-JP", false));
-        /// <summary>
-        /// 韩语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo KoreanCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ko-KR", false));
-        /// <summary>
-        /// 意大利语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo ItalianCultureInfo = CultureInfo.ReadOnly(new CultureInfo("it-IT", false));
-
-        /// <summary>
         /// 通过干员的立绘信息获取其图片
         /// </summary>
         /// <param name="illustInfo">干员的立绘信息</param>

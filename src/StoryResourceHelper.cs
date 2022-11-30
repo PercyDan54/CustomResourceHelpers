96611bca2aafa56099446aef96274339b9748789
Baka632 <stevemc123456@outlook.com>
2022-11-30 11:44:59 +0800

    删除StoryResourceHelper中不可用的成员

--- src/StoryResourceHelper.cs
+++ src/StoryResourceHelper.cs
@@ -57,27 +57,6 @@ namespace ArknightsResources.CustomResourceHelpers
     public interface IStoryResourceHelper
 	{
         /// <summary>
-        /// 简体中文的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo ChineseSimplifiedCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-CN", false));
-        /// <summary>
-        /// 繁体中文的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo ChineseTraditionalCultureInfo = CultureInfo.ReadOnly(new CultureInfo("zh-TW", false));
-        /// <summary>
-        /// 英语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo EnglishCultureInfo = CultureInfo.ReadOnly(new CultureInfo("en-US", false));
-        /// <summary>
-        /// 日语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo JapaneseCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ja-JP", false));
-        /// <summary>
-        /// 韩语的语言文化信息
-        /// </summary>
-        public static readonly CultureInfo KoreanCultureInfo = CultureInfo.ReadOnly(new CultureInfo("ko-KR", false));
-
-        /// <summary>
         /// 通过剧情文件的代号获取该剧情的原始剧情文本
         /// </summary>
         /// <param name="codename">剧情文件的代号</param>
